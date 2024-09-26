fcb3a8ca01691865f4712793d07e2379204d2301
TBA <tranbaoanh.060599@gmail.com>
2024-09-26 19:59:38 +0700

    video 21

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express"); //cu phap commomjs // import express
 const path = require("path"); //thư viện có sẵn trong nodejs nên không cần cài, dùng để sài được path.join(__dirname, "views")
-require("dotenv").config(); //import env
 const app = express(); // gán framework express cho app để tạo 1 app express
+require("dotenv").config(); //import env
 
 const port = process.env.PORT || 8888; //init port //hardcode
 const hostname = process.env.HOST_NAME;
@@ -9,6 +9,8 @@ const hostname = process.env.HOST_NAME;
 //config template engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+//config static file
+app.use(express.static(path.join(__dirname, "public")));
 
 //khai báo route
 app.get("/", (req, res) => {
@@ -17,6 +19,10 @@ app.get("/", (req, res) => {
 
 app.get("/test-ejs", (req, res) => {
   res.render("sample.ejs");
+});
+
+app.get("/test-nodemon", (req, res) => {
+  res.send("hello world with nodemon");
 });
 
 app.listen(port, hostname, () => {
