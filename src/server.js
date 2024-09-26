ecd519c6593204685ab3e44ac39dcdde486c8e9b
TBA <tranbaoanh.060599@gmail.com>
2024-09-26 11:06:36 +0700

    second

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require("express"); //cu phap commomjs // import express
+const path = require("path"); //thư viện có sẵn trong nodejs nên không cần cài, dùng để sài được path.join(__dirname, "views")
+const app = express(); // gán framework express cho app để tạo 1 app express
+const port = 8080; //init port //hardcode
+
+//config template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//khai báo route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/test-ejs", (req, res) => {
+  res.render("sample.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
