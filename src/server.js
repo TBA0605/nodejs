e4e7328d8ac1180ada283bce1ab96f4d0ccaaa88
TBA <tranbaoanh.060599@gmail.com>
2024-09-26 11:35:59 +0700

    video 19

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require("express"); //cu phap commomjs // import express
 const path = require("path"); //thư viện có sẵn trong nodejs nên không cần cài, dùng để sài được path.join(__dirname, "views")
+require("dotenv").config(); //import env
 const app = express(); // gán framework express cho app để tạo 1 app express
-const port = 8080; //init port //hardcode
+
+const port = process.env.PORT || 8888; //init port //hardcode
+const hostname = process.env.HOST_NAME;
 
 //config template engine
 app.set("views", path.join(__dirname, "views"));
@@ -16,6 +19,6 @@ app.get("/test-ejs", (req, res) => {
   res.render("sample.ejs");
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
