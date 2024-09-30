205984d0999aa786f252175fa432b69c7160c33f
TBA <tranbaoanh.060599@gmail.com>
2024-09-30 18:47:19 +0700

    video 30

--- src/server.js
+++ src/server.js
@@ -1,16 +1,31 @@
 require("dotenv").config(); //import env
 const express = require("express"); //cu phap commomjs // import express
-const path = require("path"); //thư viện có sẵn trong nodejs nên không cần cài, dùng để sài được path.join(__dirname, "views")
 const app = express(); // gán framework express cho app để tạo 1 app express
 const port = process.env.PORT || 8888; //init port //hardcode
 const hostname = process.env.HOST_NAME;
 const configViewEngine = require("./config/viewEngine");
-const webRoutes = require('./routes/web');
+const webRoutes = require("./routes/web");
+const mysql = require('mysql2');
 
 //config template engine
 configViewEngine(app);
 //khai báo route
-app.use('/',webRoutes);
+app.use("/", webRoutes);
+
+//test connection
+//create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307, //default 3306
+  user: 'root',
+  password: '123456', //default empty
+  database: 'TBA'
+});
+
+connection.query('select * from Users u', function (err, results, fields) {
+  console.log(">>>results", results); // results contains rows returned by server
+  console.log(">>>fields", fields); // fields contains extra meta data about results, if available
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
