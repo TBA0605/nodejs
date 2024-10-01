8e29b25bbe034a6f8c3e0d976102b6e3bf93359d
TBA <tranbaoanh.060599@gmail.com>
2024-10-01 10:17:16 +0700

    video 36

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,14 @@
 const connection = require("../config/database");
 
 const getHomePage = (req, res) => {
-  let users = [];
-  connection.query("select * from Users u", function (err, results, fields) {
-    users = results;
-    console.log(">>>results home page = ", results); // results contains rows returned by server
-    res.send(JSON.stringify(users));
-  });
+  // let users = [];
+  // connection.query("select * from Users u", function (err, results, fields) {
+  //   users = results;
+  //   console.log(">>>results home page = ", results); // results contains rows returned by server
+  //   res.send(JSON.stringify(users));
+  // });
+
+  return res.render("home.ejs")
 };
 
 const getTestEjs = (req, res) => {
